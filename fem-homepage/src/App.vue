3d3ec8f297ee439338d8e20b500261be961a4ae5
med-redha <redhaboutahir@gmail.com>
2022-12-10 15:14:37 +0100

    create: genreal structute

---
+++ fem-homepage/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <home-page />
+</template>
+<script>
+import HomePage from "./components/HomePage.vue";
+export default {
+  name: "App",
+  components: {
+    HomePage,
+  },
+};
+</script>
+<style lang="scss">
+@import "./style/main.scss";
+</style>
